0258c4959bc7f464c31dc367e1d6b2eaac7040bf
Montserrat <71984350+IsadoraMontserrat@users.noreply.github.com>
2025-04-27 20:26:42 -0300

    Update script-iac2.sh
    Mlehorias realizadas no script original, adicionando comandos que relatam os passos do sistema, verificação de permissões e status do Apache

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 echo "Atualizando o servidor..."
-apt-get update
-apt-get upgrade -y
-apt-get install apache2 -y
-apt-get install unzip -y
+sudo apt update -y
+sudo apt upgrade -y
 
+echo "Instalando o Apache2..."
+sudo apt install apache2 -y
 
-echo "Baixando e copiando os arquivos da aplicação..."
+echo "Instalando o Unzip..."
+sudo apt install unzip -y
 
-cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
-unzip main.zip
-cd linux-site-dio-main
-cp -R * /var/www/html/
+echo "Baixando a aplicação do GitHub..."
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip -P /tmp
 
+echo "Descompactando o arquivo..."
+unzip /tmp/main.zip -d /tmp
 
+echo "Copiando os arquivos para o diretório do Apache..."
+sudo cp -r /tmp/linux-site-dio-main/* /var/www/html/
 
+echo "Ajustando permissões para os arquivos copiados..."
+sudo chown -R www-data:www-data /var/www/html/
+
+echo "Reiniciando o Apache..."
+sudo systemctl restart apache2
+
+echo "Verificando o status do Apache..."
+sudo systemctl status apache2 | grep "active (running)"
+
+echo "Servidor web configurado e aplicação copiada com sucesso! Acesse o IP do servidor para visualizar a aplicação. :D"
